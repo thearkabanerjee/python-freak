fb448aed0028bf450dce88aa30b9f46262beee28
Arka Banerjee <arkabanerjee@Arkas-MacBook-Air.local>
2025-10-31 17:04:17 +0530

    python

---
+++ init.py
@@ -0,0 +1,16 @@
+def some_function(word):
+    space = ' ' # there is a single space between the quotes
+    if space in word:
+        return False
+    # both letters 'A' and 'Z' are in upper case
+    if not('A' <= word[0] <= 'Z'):
+        return False
+    for i in range(1, len(word)):
+        # both letters 'a' and 'z' are in lower case
+        if not('a' <= word[i] <= 'z'):
+            return False
+    return True
+
+
+a =some_function('Riemann')
+print (a)
